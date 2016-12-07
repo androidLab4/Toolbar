070c5135232c5a2d48a468b4ed824d6da05ada82
pcskl <pawel.czanasz@gmail.com>
2016-12-07 21:02:52 +0100

    init commit

---
+++ app/src/main/java/com/test/myapplication/MainActivity.java
@@ -0,0 +1,49 @@
+package com.test.myapplication;
+
+import android.content.ClipData;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity { /* When using Appcombat support library
+                                                         you need to extend Main Activity to
+                                                         ActionBarActivity.
+                                                      */
+
+
+    private Toolbar toolbar;                              // Declaring the Toolbar Object
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);                   // Setting toolbar as the ActionBar with setSupportActionBar() call
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        Toast.makeText(this, "Action selected: " + item.getTitle(),
+                Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
+
+    }
+}
